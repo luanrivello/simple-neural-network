1bb8fb802992070a07f8da4b999e9f6467334435
Luan Belem Rivello <luanrivello@hotmail.com>
2020-07-24 17:03:04 -0300

    maven init

---
+++ neural-network/src/test/java/mestre/belem/AppTest.java
@@ -0,0 +1,20 @@
+package mestre.belem;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
