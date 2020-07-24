196759a16373dd74a6f0ec2987923e065bbdae6c
Luan Belem Rivello <luanrivello@hotmail.com>
2020-07-24 17:50:10 -0300

    maven init

---
+++ neural-network/src/main/java/com/belem/App.java
@@ -0,0 +1,18 @@
+package com.belem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App{
+    public static void main( final String[] args){
+
+        System.out.println("-----------------------------START-----------------------------");
+
+        
+
+        System.out.println("------------------------------END------------------------------");
+
+    }
+
+}
