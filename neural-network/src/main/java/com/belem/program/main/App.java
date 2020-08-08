de5da96151b4064def7484910bdced51d001c363
Luan Belem Rivello <luanrivello@hotmail.com>
2020-08-08 16:52:51 -0300

    Update App.java

--- neural-network/src/main/java/com/belem/program/main/App.java
+++ neural-network/src/main/java/com/belem/program/main/App.java
@@ -10,13 +10,14 @@ public class App{
         System.out.println("-----------------------------START-----------------------------");
         
         Perceptron perceptron = new Perceptron(2);
-        double[][] data = new double[100000][2];
+        double[][] data = new double[100000][3];
         SecureRandom rd = new SecureRandom();
         
         //data
         for (int i = 0; i < data.length; i++) {
-            data[i][0] = rd.nextInt(10)-4;
-            data[i][1] = rd.nextInt(10)-4;
+            data[i][0] = rd.nextInt(5)-4;
+            data[i][1] = rd.nextInt(5)-4;
+            data[i][2] = 1;
         }
 
         //train
